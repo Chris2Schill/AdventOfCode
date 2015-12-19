958e2dba2dcfeeef1e18a63bd613abe5b3532af5
Chris <Chris2Schill@gmail.com>
2015-12-19 00:11:48 -0500

    Cleaned up code

--- Day1/FloorCounter.rb
+++ Day1/FloorCounter.rb
@@ -1,12 +1,12 @@
 floor = 0
-File.open("input.txt") do |f|
+File.open("Day1/input.txt") do |f|
     f.each_char do |c|
-        if c == '(' then floor += 1;
-        else if c == ')' then floor -= 1; 
-
-        end
+        if c == '('
+            floor += 1
+        elsif c == ')'
+            floor -= 1
         end
     end
 end
 
-print floor
+puts floor
--- Day1/FloorCounter2.rb
+++ Day1/FloorCounter2.rb
@@ -1,7 +1,7 @@
 def findPosition
     floor = 0
     charPosition = 0
-    File.open("input.txt") do |f|
+    File.open("Day1/input.txt") do |f|
         f.each_char do |c|
             if c == '('
                 floor += 1
@@ -9,11 +9,9 @@ def findPosition
                 floor -= 1
             end
             charPosition += 1
-            if floor == -1
-                return charPosition
-            end
+            return charPosition if floor == -1
         end
     end
 end
 
-print findPosition
+puts findPosition
