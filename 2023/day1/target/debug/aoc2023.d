7f7a8e6da29d8a3ac9bed81f65d652feb1ad0c74
Chris Schilling <cschilling@avtsim.com>
2023-12-04 21:37:36 +0000

    Removed more shite

--- 2023/day1/target/debug/aoc2023.d
+++
@@ -1,1 +0,0 @@
-/root/aoc/AdventOfCode/2023/day1/target/debug/aoc2023: /root/aoc/AdventOfCode/2023/day1/src/main.rs
--- 2023/day1/target/debug/deps/aoc2023-9d8062cddf227d16.d
+++
@@ -1,5 +0,0 @@
-/root/aoc/AdventOfCode/2023/target/debug/deps/aoc2023-9d8062cddf227d16: src/main.rs
-
-/root/aoc/AdventOfCode/2023/target/debug/deps/aoc2023-9d8062cddf227d16.d: src/main.rs
-
-src/main.rs:
