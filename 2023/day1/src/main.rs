be4638be25f721418e809e2ee26ff78d3d00ccf3
Chris Schilling <cschilling@avtsim.com>
2023-12-04 23:35:09 +0000

    Better impl no regex

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use regex::RegexSet;
 
 fn main() {
     let mut sum_part_one = 0;
@@ -10,25 +9,24 @@ fn main() {
         for line in lines {
             let aline = &line.unwrap();
             sum_part_one += value_part_one(aline);
-            sum_part_two += value_part_two(aline);
+            sum_part_two += value_part_two2(aline);
         }
     }
 
     println!("Sum (Part 1):{sum_part_one}\nSum (Part 2):{sum_part_two}");
-
 }
 
 fn value_part_one(line: &str) -> i32 {
     let mut value = 0;
     for c in line.chars() {
-        if c.is_digit(10) {
+        if c.is_numeric() {
             value += c.to_string().parse::<i32>().unwrap()*10;
             break;
         }
     }
 
     for c in line.chars().rev() {
-        if c.is_digit(10) {
+        if c.is_numeric() {
             value += c.to_string().parse::<i32>().unwrap();
             break;
         }
@@ -36,78 +34,57 @@ fn value_part_one(line: &str) -> i32 {
     return value;
 }
 
-fn value_part_two(line: &str) -> i32 {
 
+fn value_part_two2(line: &str) -> i32 {
+    const NUM_TEXT: [&str; 19] = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+    ];
 
-    fn find_first_digit(line: &str) -> i32 {
-        const NUMS: [&str; 19] = [
-            "zero",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-        ];
-        let set = RegexSet::new(NUMS).unwrap();
-        let mut matches: Vec<_> = set.matches(line).into_iter().map(|match_idx| &set.patterns()[match_idx]).collect();
-        matches.sort_by(|a,b| line.find(a.as_str()).unwrap().cmp(&line.find(b.as_str()).unwrap()));
-        let ordered: Vec<_> = matches.iter().map(|e| NUMS.into_iter().position(|x| x == e.to_string())).collect();
-        let mut digit = ordered[0].unwrap();
-        if digit > 9 {
-            digit -= 9;
+    let mut i = 0;
+    let mut nums = vec![];
+
+    for _c in line.chars() {
+        for num_text in NUM_TEXT {
+            let len = num_text.len();
+
+            if i+len > line.len() {
+                continue;
+            }
+
+            let slice = &line[i..(i+len)];
+            if slice == num_text {
+                let mut idx = NUM_TEXT.into_iter().position(|x| x == slice.to_string()).unwrap() as i32;
+                if idx > 9 {
+                    idx -= 9;
+                }
+                nums.push(idx);
+            }
         }
-        return digit as i32;
+        i += 1;
     }
 
-    fn find_second_digit(line: &str) -> i32 {
-        const NUMS_REV: [&str; 19] = [
-            "orez",
-            "eno",
-            "owt",
-            "eerht",
-            "ruof",
-            "evif",
-            "xis",
-            "neves",
-            "thgie",
-            "enin",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-        ];
-
-        let set_rev = RegexSet::new(NUMS_REV).unwrap();
-        let line_rev = line.chars().rev().collect::<String>();
-        let mut matches_rev: Vec<_> = set_rev.matches(&line_rev).into_iter().map(|match_idx| &set_rev.patterns()[match_idx]).collect();
-        matches_rev.sort_by(|a,b| line_rev.find(a.as_str()).unwrap().cmp(&line_rev.find(b.as_str()).unwrap()));
-        let ordered_rev: Vec<_> = matches_rev.iter().map(|e| NUMS_REV.into_iter().position(|x| x == e.to_string())).collect();
-        let mut digit = ordered_rev[0].unwrap();
-        if digit > 9 {
-            digit -= 9;
-        }
-        return digit as i32;
-    }
-
-    return find_first_digit(line)*10 + find_second_digit(line);
+    let value = nums.first().unwrap()*10 +
+                nums.last().unwrap();
+    // println!("final value for {line} = {value}");
+    return value;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
