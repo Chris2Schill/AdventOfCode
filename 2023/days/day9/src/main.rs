a650bd0b63833560f1e2e15589fddb7b9bca1177
Chris Schilling <cschilling@avtsim.com>
2023-12-15 18:13:44 +0000

    Added day9 part2

--- 2023/days/day9/src/main.rs
+++ 2023/days/day9/src/main.rs
@@ -2,11 +2,14 @@ use std::fs;
 
 fn main() {
     let sequences = parse("./days/day9/day9.in");
-    let sum: i32 = sequences.into_iter().map(|seq| predict(seq)).sum();
-    println!("Sum Predictions:{sum}");
+    let sum_next: i32 = *&sequences.iter().map(|seq| predict_next(&seq)).sum();
+    println!("Sum Next:{sum_next}");
+
+    let sum_prev: i32 = *&sequences.iter().map(|seq| predict_prev(&seq)).sum();
+    println!("Sum Prev:{sum_prev}");
 }
 
-fn predict(seq: Vec<i32>) -> i32 {
+fn predict_next(seq: &Vec<i32>) -> i32 {
 
     // base case
     let mut all_zero = true;
@@ -17,11 +20,30 @@ fn predict(seq: Vec<i32>) -> i32 {
                       .map(|w| w[1]-w[0])
                       .collect::<Vec<_>>();
 
-    println!("next = {:?}", next_seq);
+    // println!("next = {:?}", next_seq);
 
-    let prediction = predict(next_seq) + seq.last().unwrap();
+    let prediction = predict_next(&next_seq) + seq.last().unwrap();
 
-    println!(" -> {prediction}");
+    // println!(" -> {prediction}");
+    return prediction;
+}
+
+fn predict_prev(seq: &Vec<i32>) -> i32 {
+
+    // base case
+    let mut all_zero = true;
+    seq.iter().for_each(|i| all_zero &= *i == 0 );
+    if all_zero { return 0; }
+
+    let next_seq = seq.windows(2)
+                      .map(|w| w[1]-w[0])
+                      .collect::<Vec<_>>();
+
+    // println!("next = {:?}", next_seq);
+
+    let prediction = seq.first().unwrap() - predict_prev(&next_seq);
+
+    // println!(" -> {prediction}");
     return prediction;
 }
 
